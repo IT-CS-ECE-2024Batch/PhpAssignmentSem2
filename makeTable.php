d2b8fa2ac9d02b71d30a90ee1acffb4eb1fed25e
PremKarira <83239898+PremKarira@users.noreply.github.com>
2021-06-19 00:35:52 +0530

    Update makeTable.php

--- makeTable.php
+++ makeTable.php
@@ -8,36 +8,30 @@ table, th, td {
 </style>
 </head>
 <body>
-
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "8965";
+$username = "";
+$password = "";
 $dbname = "student";
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+    // change it later
 $sql = "SELECT id, std_name FROM reg";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<table><tr><th>ID</th><th>Name</th></tr>";
-    // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["std_name"]. " " . "</td></tr>";
     }
     echo "</table>";
-} else {
+} 
+else {
     echo "0 results";
 }
-
 $conn->close();
 ?>
-
 </body>
 </html>
