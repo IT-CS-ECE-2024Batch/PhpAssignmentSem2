c44149450e25b79de68f503fa0d3a3ef536524ea
tanav2202 <20U03037@iiitbhopal.ac.in>
2021-07-03 04:25:16 +0530

    small edit

--- RupeeToDollarConverter.php
+++ RupeeToDollarConverter.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversion</title>
 </head>
+
 <body>
-<div style="margin:0px auto; padding:15px; width:400px; border:1px solid black;">
+    <div style="margin:0px auto; padding:15px; width:400px; border:1px solid black;">
 
-<form action="rupee_conv.php" method="post">
-    <h2>Rupee Conversion<h2>
-    <p>
-        Enter Rupees : <input type="number" name="number">
-    </p>
-    <input type="submit" value="convert">
-    <br>
-    <br>
-    <hr>
-    <br>
-    <br>
+        <form action="RupeeTODollarConverter.php" method="post">
+            <h2>Rupee Conversion<h2>
+                    <p>
+                        Enter Rupees : <input type="number" name="number">
+                    </p>
+                    <input type="submit" value="convert">
+                    <br>
+                    <br>
+                    <hr>
+                    <br>
+                    <br>
 
-</form>
-</div>
-<?php
+        </form>
+    </div>
+    <?php
+    
 $rupee= $_POST['number'];
 $dollar = ($rupee/73.09);
 echo("Rupee in Dollar : ");
@@ -31,4 +34,5 @@ echo($dollar );
 ?>
 
 </body>
+
 </html>
